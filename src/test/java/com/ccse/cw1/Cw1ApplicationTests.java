6e5f23fa0171b382e8ae325e2b53d4f1e5f45bb3
u5556875 <louis.j.e@outlook.com>
2025-05-23 16:59:36 +0100

    Update Cw1ApplicationTests.java

--- src/test/java/com/ccse/cw1/Cw1ApplicationTests.java
+++ src/test/java/com/ccse/cw1/Cw1ApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-ci.properties")
 class Cw1ApplicationTests {
 
 	@Test
